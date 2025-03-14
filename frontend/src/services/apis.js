fcbec709789dafe0ebd1a8709edaa2ea3b6c3a9a
abhishek singh <113521742+sychoftw@users.noreply.github.com>
2025-03-14 12:59:17 +0530

    Update apis.js

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,9 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const dotenv = require("dotenv");
+
+dotenv.config();
+const BASE_URL=process.env.BASE_URL
+
+
 
 // AUTH ENDPOINTS
 export const endpoints = {
